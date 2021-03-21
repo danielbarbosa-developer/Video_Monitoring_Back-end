3a35d01cc5d854bcae761ffc281062fa7aac04fd
Daniel Barbosa <69679521+danielbarbosa-developer@users.noreply.github.com>
2021-03-21 12:33:04 -0300

    [Version 1.0.0] Integration Tests for CRUD Servers

--- Tests/WebApiIntegrationTests/ControllersTests/ApiControllerTests.cs
+++ Tests/WebApiIntegrationTests/ControllersTests/ApiControllerTests.cs
@@ -1,25 +1,105 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Backend.Application.Dtos;
 using Backend.WebApi;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
+
 namespace WebApiIntegrationTests.ControllersTests
 {
+    
     public class ApiControllerTests : IClassFixture<WebApplicationFactory<Startup>>
     {
         private readonly WebApplicationFactory<Startup> _factory;
+        private string ServerId { get; set; }
+        private string VideoId { get; set; }
 
         public ApiControllerTests()
         {
             _factory = new WebApplicationFactory<Startup>();
+            SetupTestsEnvironment();
         }
 
         [Fact]
-        public async Task Get_All_Server_Async_ReturnSuccessAndGuid()
+        public async Task Get_All_Servers_Async_ReturnsSuccessAndServersList()
         {
             var client = _factory.CreateClient();
             var response = await client.GetAsync("Api/Servers");
             response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsStringAsync();
+        }
+        [Fact]
+        public async Task Get_Server_By_Id_ReturnSuccessAndServerObject()
+        {
+            var client = _factory.CreateClient();
+            
+            var response = await client.GetAsync("Api/servers/" + ServerId);
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(result);
+            
+        }
+
+        [Fact]
+        public async Task Post_Server_Async_ReturnsSuccessAndGuid()
+        {
+            var data = new ServerDto()
+            {
+                Name = "Pier Boulevard",
+                IpAddress = "127.000.000",
+                Port = 8080
+            };
+            using var requestMessage = new HttpRequestMessage(HttpMethod.Post, "Api/server");
+            var content = await Task.FromResult(JsonSerializer.Serialize(data)).ConfigureAwait(false);
+            requestMessage.Content = new StringContent(content, Encoding.UTF8, "application/json");
+            var client = _factory.CreateClient();
+
+            var response = await client.SendAsync(requestMessage);
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(result);
+            
+        }
+        [Fact]
+        public async Task Delete_Server_Async()
+        {
+            var data = new ServerDto()
+            {
+                Name = "Pier Boulevard",
+                IpAddress = "127.000.000",
+                Port = 8080
+            };
+            using var requestMessage = new HttpRequestMessage(HttpMethod.Post, "Api/server");
+            var content = await Task.FromResult(JsonSerializer.Serialize(data)).ConfigureAwait(false);
+            requestMessage.Content = new StringContent(content, Encoding.UTF8, "application/json");
+            var client = _factory.CreateClient();
+
+            var postResponse = await client.SendAsync(requestMessage);
+            var postResult = await postResponse.Content.ReadAsStringAsync();
+            
+           
+            var response = await client.DeleteAsync("Api/server/"+postResult);
+            response.EnsureSuccessStatusCode();
+
+        }
+        private async void SetupTestsEnvironment()
+        {
+            var data = new ServerDto()
+            {
+                Name = "Pier Boulevard",
+                IpAddress = "127.000.000",
+                Port = 8080
+            };
+            using var requestMessage = new HttpRequestMessage(HttpMethod.Post, "Api/server");
+            var content = await Task.FromResult(JsonSerializer.Serialize(data)).ConfigureAwait(false);
+            requestMessage.Content = new StringContent(content, Encoding.UTF8, "application/json");
+            var client = _factory.CreateClient();
+
+            var response = await client.SendAsync(requestMessage);
+            this.ServerId = await response.Content.ReadAsStringAsync();
         }
     }
 }
