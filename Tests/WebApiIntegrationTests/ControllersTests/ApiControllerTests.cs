718fbef91fbbdde089b0e2902096e62c881c75c9
Daniel Barbosa <69679521+danielbarbosa-developer@users.noreply.github.com>
2021-03-22 14:42:36 -0300

    [Version 1.0.0] Creating BaseControllerTest and more tests

--- Tests/WebApiIntegrationTests/ControllersTests/ApiControllerTests.cs
+++ Tests/WebApiIntegrationTests/ControllersTests/ApiControllerTests.cs
@@ -5,40 +5,41 @@ using System.Threading.Tasks;
 using Backend.Application.Dtos;
 using Backend.Application.Dtos.Input;
 using Backend.Application.Handlers;
+using Backend.Domain.Entities;
 using Backend.WebApi;
 using Microsoft.AspNetCore.Mvc.Testing;
+using WebApiIntegrationTests.Setup;
 using Xunit;
+
 
 
 namespace WebApiIntegrationTests.ControllersTests
 {
     
-    public class ApiControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    public class ApiControllerTests : BaseControllerTest, IClassFixture<WebApplicationFactory<Startup>>
     {
         private readonly WebApplicationFactory<Startup> _factory;
-        private string ServerId { get; set; }
-        private string VideoId { get; set; }
-
         public ApiControllerTests()
         {
             _factory = new WebApplicationFactory<Startup>();
-            SetupTestsEnvironment();
         }
 
         [Fact]
         public async Task Get_All_Servers_Async_ReturnsSuccessAndServersList()
         {
             var client = _factory.CreateClient();
-            var response = await client.GetAsync("Api/Servers");
+            var response = await client.GetAsync("Api/servers");
             response.EnsureSuccessStatusCode();
             var result = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(result);
         }
         [Fact]
         public async Task Get_Server_By_Id_ReturnSuccessAndServerObject()
         {
+            string serverId = await SetupServerToTest(_factory);
             var client = _factory.CreateClient();
             
-            var response = await client.GetAsync("Api/servers/" + ServerId);
+            var response = await client.GetAsync("Api/servers/" + serverId);
             response.EnsureSuccessStatusCode();
             var result = await response.Content.ReadAsStringAsync();
             Assert.NotEmpty(result);
@@ -81,46 +82,51 @@ namespace WebApiIntegrationTests.ControllersTests
 
             var postResponse = await client.SendAsync(requestMessage);
             var postResult = await postResponse.Content.ReadAsStringAsync();
-            
-           
             var response = await client.DeleteAsync("Api/server/"+postResult);
             response.EnsureSuccessStatusCode();
-
         }
-
         [Fact]
         public async Task Post_Video_Async_ReturnsSuccessAndGuid()
         {
+            string serverId = await SetupServerToTest(_factory);
             var base64String = VideoFileHandler.ConvertVideoToBase64("../../../../TestsData/KrabVideoTest.mp4");
             var data = new VideoDtoInput()
             {
                 Description = "A Krab fishing video", //only for tests
                 VideoContent = base64String
             };
-            using var requestMessage = new HttpRequestMessage(HttpMethod.Post, "Api/servers/"+ServerId+"/videos");
+            using var requestMessage = new HttpRequestMessage(HttpMethod.Post, "Api/servers/"+serverId+"/videos");
             var content = await Task.FromResult(JsonSerializer.Serialize(data)).ConfigureAwait(false);
             requestMessage.Content = new StringContent(content, Encoding.UTF8, "application/json");
             var client = _factory.CreateClient();
 
             var response = await client.SendAsync(requestMessage);
-            this.VideoId = await response.Content.ReadAsStringAsync();
-            
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(result);
+            Assert.NotNull(result);
+            await ClearVideoData(_factory, result);
+
         }
-        private async void SetupTestsEnvironment()
+        [Fact]
+        public async Task Download_Video_From_Server_Async_ReturnSuccessAndBase64()
         {
-            var data = new ServerDto()
-            {
-                Name = "Pier Boulevard",
-                IpAddress = "127.000.000",
-                Port = 8080
-            };
-            using var requestMessage = new HttpRequestMessage(HttpMethod.Post, "Api/server");
-            var content = await Task.FromResult(JsonSerializer.Serialize(data)).ConfigureAwait(false);
-            requestMessage.Content = new StringContent(content, Encoding.UTF8, "application/json");
             var client = _factory.CreateClient();
+            var response = await client.GetAsync("Api/servers/00facf75-4984-4f11-80a6-e8a491d1b489/videos/aeebe28b-317f-45be-a072-8c3d89f095c7/binary");
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(result);
+            Assert.NotNull(result);
+        }
 
-            var response = await client.SendAsync(requestMessage);
-            this.ServerId = await response.Content.ReadAsStringAsync();
+        [Fact]
+        public async Task Delete_Video_From_Server_Async_ReturnSuccess()
+        {
+            var videoToDelete = SetupVideoToTest(_factory);
+            var client = _factory.CreateClient();
+            
+            var response = await client.DeleteAsync($"Api/servers/00facf75-4984-4f11-80a6-e8a491d1b489/videos/{videoToDelete}");
+            response.EnsureSuccessStatusCode();
         }
     }
 }
---
+++ Tests/WebApiIntegrationTests/Setup/BaseControllerTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Backend.Application.Dtos;
+using Backend.Application.Dtos.Input;
+using Backend.Application.Handlers;
+using Backend.WebApi;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace WebApiIntegrationTests.Setup
+{
+    public class BaseControllerTest
+    {
+        protected async Task<string> SetupServerToTest( WebApplicationFactory<Startup> factory)
+        {
+            var data = new ServerDto()
+            {
+                Name = "Pier Boulevard",
+                IpAddress = "127.000.000",
+                Port = 8080
+            };
+            using var requestMessage = new HttpRequestMessage(HttpMethod.Post, "Api/server");
+            var content = await Task.FromResult(JsonSerializer.Serialize(data)).ConfigureAwait(false);
+            requestMessage.Content = new StringContent(content, Encoding.UTF8, "application/json");
+            var client = factory.CreateClient();
+
+            var response = await client.SendAsync(requestMessage);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        protected async Task<string> SetupVideoToTest(WebApplicationFactory<Startup> factory)
+        {
+            string serverId = await SetupServerToTest(factory);
+            var base64String = VideoFileHandler.ConvertVideoToBase64("../../../../TestsData/KrabVideoTest.mp4");
+            var data = new VideoDtoInput()
+            {
+                Description = "A Krab fishing video", //only for tests
+                VideoContent = base64String
+            };
+            using var requestMessage = new HttpRequestMessage(HttpMethod.Post, "Api/servers/"+serverId+"/videos");
+            var content = await Task.FromResult(JsonSerializer.Serialize(data)).ConfigureAwait(false);
+            requestMessage.Content = new StringContent(content, Encoding.UTF8, "application/json");
+            var client = factory.CreateClient();
+
+            var response = await client.SendAsync(requestMessage);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        protected async Task ClearVideoData(WebApplicationFactory<Startup> factory, string videoToDelete)
+        {
+            var client = factory.CreateClient();
+            await client.DeleteAsync($"Api/servers/00facf75-4984-4f11-80a6-e8a491d1b489/videos/{videoToDelete}");
+        }
+    }
+}
